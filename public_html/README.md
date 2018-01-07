aa78bb91aa0fe7eeb2a23df8fae260cceb9d40cd
Tanguy CHENIER <tanguychenier@gmail.com>
2018-01-07 23:08:37 +0100

    Add: Initial commit

---
+++ public_html/README.md
@@ -0,0 +1,21 @@
+JUST SQUARES
+### About
+
+```
+This game can be used in browser with WebGL tech. and only used for personnal 
+research.
+
+It's a first person shooter with object interaction.
+
+You can download it and just launch the "index.html" file to see the result
+![alt text](https://github.com/cerb3re/justSquare_WebGL/assets/images/ingame.jpg)
+```
+
+## Actual version
+
+- 1: Just print some object in 3D
+- 2: coming soon, will have a 3D player interaction 
+
+## From
+
+- t.chenier: `2018`
---
+++ public_html/js/Arena.js
@@ -0,0 +1,39 @@
+/*
+ * t.chenier Arena engine
+ * here are the graphical object values.
+ */
+
+Arena = function(game) {
+    this.game       = game;
+    
+    var scene       = game.scene;
+    var light       = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 1, 0), scene);
+    var ground      = new BABYLON.Mesh.CreateGround("ground1", 20, 20, 2, scene);
+    var mainBox     = BABYLON.Mesh.CreateBox("box1", 3, scene);
+    var mainBox2    = mainBox.clone("box2"); 
+    var mainBox3    = mainBox.clone("box3"); 
+    var mainBox4    = mainBox.clone("box4");
+    var cylinder    = BABYLON.Mesh.CreateCylinder("cyl1", 20, 5, 5, 20, 4, scene);
+    
+    ground.scaling  = new BABYLON.Vector3(2, 2, 3);
+    
+    // initial size: 3
+    // every initial scale object are mapped by there vertex center and must be 
+    // multiplie by twice of there initial size to get mapped into the floor.
+    mainBox.scaling.y   = 1;
+    mainBox.position    = new BABYLON.Vector3(5,((3 / 2) * mainBox.scaling.y), 5);
+    mainBox.rotation.y  = (Math.PI * 45 / 180);
+    
+    mainBox2.scaling.y = 2;
+    mainBox2.position = new BABYLON.Vector3(5,((3/2)*mainBox2.scaling.y),-5);
+
+    mainBox3.scaling.y = 3;
+    mainBox3.position = new BABYLON.Vector3(-5,((3/2)*mainBox3.scaling.y),-5);
+
+    mainBox4.scaling.y = 4;
+    mainBox4.position = new BABYLON.Vector3(-5,((3/2)*mainBox4.scaling.y),5);
+
+    cylinder.position.y = 20/2;
+
+};
+
