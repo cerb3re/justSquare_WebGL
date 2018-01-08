8c8fdfa95bab88a0fbbd29bac5795d8d445e81ee
Tanguy CHENIER <tanguychenier@gmail.com>
2018-01-08 03:50:58 +0100

    Add: Angular / Rad / Deg movement for camera player

--- public_html/js/Player.js
+++ public_html/js/Player.js
@@ -1,22 +1,72 @@
-Player = function(game, canvas) {
-    // La scène du jeu
-    this.scene = game.scene;
+/*
+ * t.chenier 2018
+ */
 
-    // Initialisation de la caméra
-    this._initCamera(this.scene, canvas);
+Player = function(game, canvas) {
+    // Camera access inside player view
+    var _this = this;
     
+    this.game = game;
+    // initiate the maximum angular vision angle
+    this.angularSensibility = 999.888;
+    this.axisMovement = [false, false, false, false];
+    window.addEventListener("keyup", function(e){
+        switch(e.keyCode) {
+            case 38:
+                _this.axisMovement[0] = false;
+                break;
+            case 40:
+                _this.axisMovement[1] = false;
+                break;            
+            case 37:
+                _this.axisMovement[2] = false;
+                break;            
+            case 39:
+                _this.axisMovement[3] = false;
+                break;
+        }
+    }, false);
+    window.addEventListener("keydown", function(e){
+        switch(e.keyCode) {
+            case 38:
+                console.log("38 : up - keydown");
+                _this.axisMovement[0] = true;
+                break;
+            case 40:
+                _this.axisMovement[1] = true;
+                break;            
+            case 37:
+                _this.axisMovement[2] = true;
+                break;            
+            case 39:
+                _this.axisMovement[3] = true;
+                break;
+        }
+    }, false);
+    // change the camera movement angle in function of the
+    // axis mouse movement in a dynamic way.
+    window.addEventListener("mousemove", function(e) {
+       _this.camera.rotation.y += e.movementX * 0.001 * (_this.angularSensibility / 250);
+       console.log(e.movementX);
+       var nextRotationX = _this.camera.rotation.x + (e.movementY * 0.001 * (_this.angularSensibility / 250));
+       
+       if (nextRotationX < degToRad(90) && nextRotationX > degToRad(-90)) {
+           _this.camera.rotation.x += e.movementY * 0.001 * (_this.angularSensibility / 250);
+       }
+    }, false);
+    
+    // Initialisation de la caméra
+    this._initCamera(this.game.scene, canvas);
 };
 
 Player.prototype = {
     _initCamera : function(scene, canvas) {
         // On crée la caméra
-        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
-
-        // On demande à la caméra de regarder au point zéro de la scène
+        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(-20, 5, 0), scene);
+        this.camera.axisMovement = [false, false, false, false];
+        this.isAlive = true;
         this.camera.setTarget(BABYLON.Vector3.Zero());
 
-        // On affecte le mouvement de la caméra au canvas
-        this.camera.attachControl(canvas, true);
     }
 };
 var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
