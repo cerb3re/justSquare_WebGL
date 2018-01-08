c4c237d319d5ddba16ce34541c6f257fab6c8c89
Tanguy CHENIER <tanguychenier@gmail.com>
2018-01-08 02:26:04 +0100

    Add: Real first Arena

--- public_html/js/Arena.js
+++ public_html/js/Arena.js
@@ -1,62 +1,49 @@
-/*
- * t.chenier Arena engine
- * here are the graphical object values.
- */
-
 Arena = function(game) {
-    this.game       = game;
-    
-    var materialGround = new BABYLON.StandardMaterial("groundTexture", scene);
-    materialGround.diffuseTexture = new BABYLON.Texture("assets/images/floor.jpg", scene);
-    // uScale : height
-    materialGround.diffuseTexture.uScale = 4.0;
-    materialGround.diffuseTexture.vScale = 4.0;
-    
-    var materialObject = new BABYLON.StandardMaterial("objectTexture", scene);
-    materialObject.diffuseTexture = new BABYLON.Texture("assets/images/object.jpg");
-    
-    var scene       = game.scene;
-    var light       = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 20, 0), scene);
-    var ground      = new BABYLON.Mesh.CreateGround("ground1", 20, 20, 2, scene);   
-    var mainBox     = BABYLON.Mesh.CreateBox("box1", 3, scene);
-    var mainBox2    = mainBox.clone("box2"); 
-    var mainBox3    = mainBox.clone("box3"); 
-    var mainBox4    = mainBox.clone("box4");
-    var cylinder    = BABYLON.Mesh.CreateCylinder("cyl1", 20, 5, 5, 20, 4, scene);
-    
-    // diffuse: color
-    light.diffuse       = new BABYLON.Color3(1, 1, 1);
-    // specular: intensity
-    light.specular     = new BABYLON.Color3(1, 1, 1);
-  
-    // initial size: 3
-    // every initial scale object are mapped by there vertex center and must be 
-    // multiplie by the double of there divided center to be on the floor.
-    mainBox.scaling.y   = 1;
-    mainBox.position    = new BABYLON.Vector3(5,((3 / 2) * mainBox.scaling.y), 5);
-    mainBox.rotation.y  = (Math.PI * 45 / 180);
-    mainBox.material    = materialObject;
-    
-    mainBox2.scaling.y  = 2;
-    mainBox2.position   = new BABYLON.Vector3(5,((3/2)*mainBox2.scaling.y),-5);
-    mainBox2.rotation.x = mainBox.rotation.y;
-    mainBox2.material   = materialObject;
+    // Appel des variables nécéssaires
+    this.game = game;
+    var scene = game.scene;
 
+    // Création de notre lumière principale
+    var light = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(0, 10, 0), scene);
+    var light2 = new BABYLON.HemisphericLight("light2", new BABYLON.Vector3(0, -1, 0), scene);
+    light2.intensity = 0.8;
 
-    mainBox3.scaling.y  = 3;
-    mainBox3.position   = new BABYLON.Vector3(-5,((3/2)*mainBox3.scaling.y),-5);
-    mainBox3.material   = materialObject;
+    // Material pour le sol
+    var materialGround = new BABYLON.StandardMaterial("wallTexture", scene);
+    materialGround.diffuseTexture = new BABYLON.Texture("assets/images/tile.jpg", scene);
+    materialGround.diffuseTexture.uScale = 8.0;
+    materialGround.diffuseTexture.vScale = 8.0;
 
-    mainBox4.scaling.y  = 4;
-    mainBox4.position   = new BABYLON.Vector3(-5,((3/2)*mainBox4.scaling.y),5);
-    mainBox4.material   = materialObject;
+    // Material pour les objets
+    var materialWall = new BABYLON.StandardMaterial("groundTexture", scene);
+    materialWall.diffuseTexture = new BABYLON.Texture("assets/images/tile.jpg", scene);
 
-    cylinder.position.y = 20/2;
-    cylinder.material   = materialObject;
-    
-    ground.scaling      = new BABYLON.Vector3(2, 10, 3);
-    ground.scaling.z    = 2;
-    ground.material     = materialGround;
+    var boxArena = BABYLON.Mesh.CreateBox("box1", 100, scene, false, BABYLON.Mesh.BACKSIDE);
+    boxArena.material = materialGround;
+    boxArena.position.y = 50 * 0.3;
+    boxArena.scaling.y = 0.3;
+    boxArena.scaling.z = 0.8;
+    boxArena.scaling.x = 3.5;
 
+    var columns = [];
+    var numberColumn = 6;
+    var sizeArena = 100*boxArena.scaling.x -50;
+    var ratio = ((100/numberColumn)/100) * sizeArena;
+    for (var i = 0; i <= 1; i++) {
+        if(numberColumn>0){
+            columns[i] = [];
+            let mainCylinder = BABYLON.Mesh.CreateCylinder("cyl0-"+i, 30, 5, 5, 20, 4, scene);
+            mainCylinder.position = new BABYLON.Vector3(-sizeArena/2,30/2,-20 + (40 * i));
+            mainCylinder.material = materialWall;
+            columns[i].push(mainCylinder);
+
+            if(numberColumn>1){
+                for (let y = 1; y <= numberColumn - 1; y++) {
+                    let newCylinder = columns[i][0].clone("cyl"+y+"-"+i);
+                    newCylinder.position = new BABYLON.Vector3(-(sizeArena/2) + (ratio*y),30/2,columns[i][0].position.z);
+                    columns[i].push(newCylinder);
+                }
+            }
+        }
+    }
 };
-
--- public_html/js/Player.js
+++ public_html/js/Player.js
@@ -19,3 +19,5 @@ Player.prototype = {
         this.camera.attachControl(canvas, true);
     }
 };
+var camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", 1, 0.8, 10, new BABYLON.Vector3(0, 0, 0), scene);
+camera.attachControl(canvas, true);
